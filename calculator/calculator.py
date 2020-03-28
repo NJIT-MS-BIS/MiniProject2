36b28d2f0e13e14e27221d6c9831a59b4ccc9066
amanda <ako9@njit.edu>
2020-03-28 13:41:31 -0400

    [amanda] - updated calculator module

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,17 +1,31 @@
 from calculator.subtraction import subtraction
 from calculator.addition import addition
-
+from calculator.division import division
+from calculator.multiplication import multiplication
 
 class Calculator:
     result = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, result):
+        self.result = result
 
-    def add(self, a, b):
-        self.result = addition(a, b)
-        return self.result
+    @staticmethod
+    def add(num_one, num_two):
+        result = addition(num_one, num_two)
+        return result
 
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.result
+    @staticmethod
+    def subtract(num_one, num_two):
+        result = subtraction(num_one, num_two)
+        return result
+
+    @staticmethod
+    def multiply(num_one, num_two):
+        result = multiplication(num_one, num_two)
+        return result
+
+    @staticmethod
+    def divide(num_one, num_two):
+        result = division(num_one, num_two)
+        return result
+

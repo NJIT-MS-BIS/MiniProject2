cdf307f3937b5488e086313017e78bdb176bc141
amanda <ako9@njit.edu>
2020-03-28 16:27:02 -0400

    [amanda] Made changes to main statistics module

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -6,14 +6,7 @@ class Statistics(Calculator):
 
 
 
-    @staticmethod
-    def variance(data):
-        mu = mean(data)
-        return mean([(x - mu) ** 2 for x in data])
 
-    @staticmethod
-    def stddev(data):
-        return sq_root(variance(data))
 
     @staticmethod
     def quartile(data):

455335f6f957a03af044a9fff971b4a65732c6f3
ahusanov <a.khusanov@gmail.com>
2020-03-29 18:00:54 -0400

    [akmal] Fix: refactored median module

--- src/statistics/median.py
+++ src/statistics/median.py
@@ -1,11 +1,19 @@
-def median(data):
-    """Pass a list"""
-    n = len(data)
-    data.sort()
-    if n % 2 == 0:
-        first_half = data[n // 2]
-        second_half = data[n // 2 - 1]
-        median = (first_half + second_half) / 2
-    else:
-        median = data[n // 2]
-    return median
+from calculator.addition import addition
+from calculator.division import division
+from calculator.subtraction import subtraction
+
+
+def median(num_list: list):
+    """returns the median of list. List must be passed as an argument."""
+    try:
+        n = len(num_list)
+        num_list.sort()
+        if n % 2 == 0:
+            first_half = num_list[int(n // 2)]
+            second_half = num_list[int(subtraction(n // 2, 1))]
+            median = division(addition(first_half, second_half), 2)
+        else:
+            median = num_list[int(division(n, 2))]
+        return median
+    except ZeroDivisionError as err:
+        print(err)

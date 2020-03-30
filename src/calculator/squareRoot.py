8d35ada658585a20b82a1721ac5fda022bf977f0
ahusanov <a.khusanov@gmail.com>
2020-03-30 02:33:05 -0400

    [akmal] fix: run time errors

--- src/calculator/squareRoot.py
+++ src/calculator/squareRoot.py
@@ -1,8 +1,2 @@
 def squarerooting(num):
-    try:
-        if type(num) is not None:
-            return round((float(num) ** 0.5), 8)
-        else:
-            return float(0)
-    except (ValueError, TypeError):
-        print ("Error: Check your data inputs")
+    return round((float(num) ** 0.5), 8)

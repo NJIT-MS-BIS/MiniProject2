5e1057e90a53de66a61ce3a57af874300ff900bb
amanda <ako9@njit.edu>
2020-03-30 17:57:32 -0400

    [amanda] Update: Created skewness module

---
+++ src/statistics/skewness.py
@@ -0,0 +1,17 @@
+from src.statistics.mean import mean
+from src.statistics.median import median
+from src.statistics.stddev import std_dev
+
+
+def skewness(data: list):
+    try:
+
+        mean_data = mean(data)
+        median_data = median(data)
+        stddev_data = std_dev(data)
+        skewness = 3 * (mean_data - median_data) / stddev_data
+        return skewness
+    except ZeroDivisionError:
+        print("Error: Can't Divide by 0")
+    except ValueError:
+        print("Error: Check your data inputs")

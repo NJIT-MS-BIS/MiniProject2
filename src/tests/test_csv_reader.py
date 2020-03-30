ee03238d7c6acf906650eb23a30a6e2ba3f45fe3
ahusanov <a.khusanov@gmail.com>
2020-03-29 21:32:19 -0400

    [akmal] Feature: csv reader test is added with data

---
+++ src/tests/test_csv_reader.py
@@ -0,0 +1,19 @@
+import unittest2
+from src.utilities.csvHelper import class_factory, CsvReader
+
+
+class CsvTestCase(unittest2.TestCase):
+
+    def setUp(self) -> None:
+        self.csv_reader = CsvReader('../../data/UnitTestAddition.csv')
+
+    def test_return_data_as_objects(self):
+        people = self.csv_reader.return_data_as_class('Value 1')
+        self.assertIsInstance(people, list)
+        test_class = class_factory('Value 1', self.csv_reader.data[0])
+        for person in people:
+            self.assertEqual(person.__name__, test_class.__name__)
+
+
+if __name__ == '__main__':
+    unittest2.main()

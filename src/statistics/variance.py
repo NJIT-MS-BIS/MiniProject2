cf12319a50b4b66f2f88d16b54db9cbc7319fda1
ahusanov <a.khusanov@gmail.com>
2020-03-30 00:50:22 -0400

    [akmal] Fix: renamed

---
+++ src/statistics/variance.py
@@ -0,0 +1,15 @@
+from src.calculator.square import squaring
+from src.calculator.division import division
+from src.statistics.mean import mean
+
+
+def variance(num: list):
+    try:
+        pop_mean = mean(num)
+        num_values = len(num)
+        x = 0
+        for i in num:
+            x = x + squaring(i-pop_mean)
+        return division(x, num_values)
+    except ZeroDivisionError as err:
+        print(err)
--- src/statistics/varience.py
+++
@@ -1,6 +0,0 @@
-from statistics.mean import mean
-
-
-def variance(data):
-    mu = mean(data)
-    return mean([(x - mu) ** 2 for x in data])
